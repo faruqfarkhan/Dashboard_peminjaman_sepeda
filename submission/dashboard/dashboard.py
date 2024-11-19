586d697680985eb3c332e6d70eaf60eadb7c83e8
faruqfarkhan <88619667+faruqfarkhan@users.noreply.github.com>
2024-11-19 22:22:06 +0700

    Update dashboard.py

--- submission/dashboard/dashboard.py
+++ submission/dashboard/dashboard.py
@@ -14,7 +14,7 @@ import streamlit as st
 
 
 # file = "subbmission/data/day_data.csv"
-day_data = pd.read_csv("day_data.csv")
+day_data = pd.read_csv("submission/dashboard/day_data.csv")
 day_data["dteday"] = pd.to_datetime(day_data["dteday"])
 
 
